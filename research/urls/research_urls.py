d649404a54e47b6fb6fcf7a5bdfa7156ef2f725f
awoiibobwilly <awoiibobwilly@gmail.com>
2026-07-09 14:15:07 +0300

    research: added urls

--- research/urls/research_urls.py
+++ research/urls/research_urls.py
@@ -3,28 +3,25 @@ from django.urls import path
 from ..views import (
     ResearchListAPIView,
     ResearchDetailAPIView,
+    ResearchPageAPIView,
 )
 
 urlpatterns = [
+    path(
+        "page/",
+        ResearchPageAPIView.as_view(),
+        name="research-page",
+    ),
 
     path(
-
         "",
-
         ResearchListAPIView.as_view(),
-
         name="research-list",
-
     ),
 
     path(
-
         "<slug:slug>/",
-
         ResearchDetailAPIView.as_view(),
-
         name="research-detail",
-
     ),
-
 ]
